eb980de3809cd0b85a43beb454abed2914ef051c
lyellread <lyellread@gmail.com>
2020-07-28 21:35:07 -0700

    Linting

--- YTMP3 CSV Downloader.py
+++ YTMP3 CSV Downloader.py
@@ -1,4 +1,4 @@
-#This is for https://ytmp3.cc/en/
+# This is for https://ytmp3.cc/en/
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,58 +12,56 @@ from selenium.webdriver.support import expected_conditions
 
 fname = input("Enter downloaded file name (without txt entention): ")
 
-with open(fname + ".txt", 'r') as f:
+with open(fname + ".txt", "r") as f:
     for line in f:
 
-            
         temp = line
-        temp = temp.replace("\"","")
-        temp = temp.replace(","," ")
-        temp = temp.replace("-","")
-        temp = temp.replace("  "," ")
-        #Parsed input from txt document
-        
-        browser = webdriver.Chrome()
-        browser.get('http://www.google.com')
-        search = browser.find_element_by_name('q')
-        search.send_keys("youtube " + temp)
-        result = browser.find_elements_by_xpath('//div[@class="r"]/a/h3')        
-        result[0].click()
-        #Opened first search result
+        temp = temp.replace('"', "")
+        temp = temp.replace(",", " ")
+        temp = temp.replace("-", "")
+        temp = temp.replace("  ", " ")
+        # Parsed input from txt document
 
+        browser = webdriver.Chrome()
+        browser.get("http://www.google.com")
+        search = browser.find_element_by_name("q")
+        search.send_keys("youtube " + temp)
+        result = browser.find_elements_by_xpath('//div[@class="r"]/a/h3')
+        result[0].click()
+        # Opened first search result
 
         URL = browser.current_url
-        URL = URL.replace("https://","")
-        if(URL[:15] == "www.youtube.com"):#14
+        URL = URL.replace("https://", "")
+        if URL[:15] == "www.youtube.com":  # 14
             try:
-                #Copy URL from search result and check to make sure Youtube
+                # Copy URL from search result and check to make sure Youtube
 
                 browser.get("https://ytmp3.cc/en/")
-                #Navigate to Youtube to MP3 Converter
+                # Navigate to Youtube to MP3 Converter
 
-                
-                search = browser.find_element_by_id('input')
+                search = browser.find_element_by_id("input")
                 search.send_keys(URL)
                 search = browser.find_element_by_id("submit")
                 search.send_keys(Keys.RETURN)
                 time.sleep(30)
-                
+
                 try:
-                    search = browser.find_element_by_xpath("/html/body/div[2]/div[1]/div[1]/div[3]/a[1]").click()
+                    search = browser.find_element_by_xpath(
+                        "/html/body/div[2]/div[1]/div[1]/div[3]/a[1]"
+                    ).click()
                     time.sleep(10)
-                    print ("Song " + temp + " successfully downloading")
+                    print("Song " + temp + " successfully downloading")
                     browser.quit()
                 except:
                     print("Song " + temp + " failed. Closing and moving on")
                     browser.quit()
-                #Find entry bar, enter URL, wait, download
+                # Find entry bar, enter URL, wait, download
 
-            #except ElementNotInteractableException as exception:
-             #    print ("Song " + temp + " failed due to unclickable element")
-              #   browser.quit()
+            # except ElementNotInteractableException as exception:
+            #    print ("Song " + temp + " failed due to unclickable element")
+            #   browser.quit()
             except:
                 browser.quit()
-        #Close the browser and iterate to next entry
-        
-        
-    #Just an extra line!
+        # Close the browser and iterate to next entry
+
+    # Just an extra line!
