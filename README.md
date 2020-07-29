d84575e61bcd3bc5d7faa1fd6d40054f5255e41d
lyellread <lyellread@gmail.com>
2020-07-28 21:52:05 -0700

    Adding to README, neatening function of Downloader

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # spotify-playlist-downloader
 Downloades songs from Spotify playlist
 
-Prerequisites: 
-- Must have PIP installed
-- Python must be on path
+## Prerequisites
+
+- `pip3`
+- [`youtube-dl`](https://github.com/ytdl-org/youtube-dl/blob/master/README.md#installation)
+- [chromedriver](https://chromedriver.chromium.org/) in path
+
+## Usage
+
 - Must convert Spotify to CSV document (in .txt form) using program such as https://www.playlist-converter.net/#/
-- Must have Chrome web driver and .txt document in folder in same folder as program
--- YTMP3 CSV Downloader.py
+++ YTMP3 CSV Downloader.py
@@ -1,4 +1,5 @@
 # This is for https://ytmp3.cc/en/
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,59 +10,65 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions
 
+# TODO: use youtube-dl
 
 fname = input("Enter downloaded file name (without txt entention): ")
 
 with open(fname + ".txt", "r") as f:
-    for line in f:
 
-        temp = line
-        temp = temp.replace('"', "")
-        temp = temp.replace(",", " ")
-        temp = temp.replace("-", "")
-        temp = temp.replace("  ", " ")
-        # Parsed input from txt document
+    songs = f.readlines()
 
+# Parse and clean up song names from file
+songs = [x.strip('"-').replace(",", " ").replace("  ", " ") for x in songs]
+
+for song in songs:
+
+    # Open a webdriver
+    try:
         browser = webdriver.Chrome()
         browser.get("http://www.google.com")
-        search = browser.find_element_by_name("q")
-        search.send_keys("youtube " + temp)
-        result = browser.find_elements_by_xpath('//div[@class="r"]/a/h3')
-        result[0].click()
-        # Opened first search result
+    except:
+        print("Failed to open chromedriver")
+        exit(-1)
 
-        URL = browser.current_url
-        URL = URL.replace("https://", "")
-        if URL[:15] == "www.youtube.com":  # 14
+    # Search google for the songname
+    search = browser.find_element_by_name("q")
+    search.send_keys("youtube " + song)
+    result = browser.find_elements_by_xpath('//div[@class="r"]/a/h3')
+
+    # Select the first result
+    result[0].click()
+
+    if "www.youtube.com" in browser.current_url:
+        try:
+            # Copy URL from search result and check to make sure Youtube
+
+            browser.get("https://ytmp3.cc/en/")
+            # Navigate to Youtube to MP3 Converter
+
+            search = browser.find_element_by_id("input")
+            search.send_keys(URL)
+            search = browser.find_element_by_id("submit")
+            search.send_keys(Keys.RETURN)
+            time.sleep(30)
+
             try:
-                # Copy URL from search result and check to make sure Youtube
-
-                browser.get("https://ytmp3.cc/en/")
-                # Navigate to Youtube to MP3 Converter
-
-                search = browser.find_element_by_id("input")
-                search.send_keys(URL)
-                search = browser.find_element_by_id("submit")
-                search.send_keys(Keys.RETURN)
-                time.sleep(30)
-
-                try:
-                    search = browser.find_element_by_xpath(
-                        "/html/body/div[2]/div[1]/div[1]/div[3]/a[1]"
-                    ).click()
-                    time.sleep(10)
-                    print("Song " + temp + " successfully downloading")
-                    browser.quit()
-                except:
-                    print("Song " + temp + " failed. Closing and moving on")
-                    browser.quit()
-                # Find entry bar, enter URL, wait, download
-
-            # except ElementNotInteractableException as exception:
-            #    print ("Song " + temp + " failed due to unclickable element")
-            #   browser.quit()
-            except:
+                search = browser.find_element_by_xpath(
+                    "/html/body/div[2]/div[1]/div[1]/div[3]/a[1]"
+                ).click()
+                time.sleep(10)
+                print("Song " + song + " successfully downloading")
                 browser.quit()
-        # Close the browser and iterate to next entry
+            except:
+                print("Song " + song + " failed. Closing and moving on")
+                browser.quit()
+            # Find entry bar, enter URL, wait, download
+
+        # except ElementNotInteractableException as exception:
+        #    print ("Song " + temp + " failed due to unclickable element")
+        #   browser.quit()
+        except:
+            browser.quit()
+    # Close the browser and iterate to next entry
 
     # Just an extra line!
